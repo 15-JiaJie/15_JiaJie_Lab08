6f1bb4baaafaf623a76812aea4f818284e57b5c7
29-EricTu <erictujiaxin48@gmail.com>
2024-01-22 12:01:49 +0800

    Updated

--- Assets/Scripts/CalculatorScript.cs
+++ Assets/Scripts/CalculatorScript.cs
@@ -11,6 +11,10 @@ public class CalculatorScript : MonoBehaviour
     public InputField ValueText;
     public Toggle US_Dollars;
     public Toggle JP_Yen;
+    public Toggle MY_RM;
+    public Toggle EU_EUR;
+    public Toggle KR_KRW;
+    public Toggle TW_TWD;
     public Text ErrorText;
 
     // Start is called before the first frame update
@@ -50,6 +54,30 @@ public class CalculatorScript : MonoBehaviour
         {
             Value = SGDamount * 110.5f;
             ValueText.text = Value.ToString() + "JPY";
+        }
+
+        if (MY_RM.GetComponent<Toggle>().isOn == true)
+        {
+            Value = SGDamount * 3.08f;
+            ValueText.text = Value.ToString() + "RM";
+        }
+
+        if (EU_EUR.GetComponent<Toggle>().isOn == true)
+        {
+            Value = SGDamount * 0.63f;
+            ValueText.text = Value.ToString() + "EUR";
+        }
+
+        if (KR_KRW.GetComponent<Toggle>().isOn == true)
+        {
+            Value = SGDamount * 881.54f;
+            ValueText.text = Value.ToString() + "KRW";
+        }
+
+        if (TW_TWD.GetComponent<Toggle>().isOn == true)
+        {
+            Value = SGDamount * 20.73f;
+            ValueText.text = Value.ToString() + "TWD";
         }
 
         if ((US_Dollars.isOn) && (JP_Yen.isOn))
